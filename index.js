cc4e66d3f250962fd2f1b87d901dd021c7c26a74
Élisson Michael <elissonmichael@yahoo.com.br>
2022-10-17 11:33:26 -0300

    Add Radio Button Update All Method

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-function calculateLCI(element) {
+document.addEventListener('DOMContentLoaded', function() {
+  M.AutoInit(); // Initialize all of the Materialize Components
+});
+
+function lciValue() {
+  return document.querySelector('input[id="lci"]').value;
+}
+
+function cdbValue() {
+  return document.querySelector('input[id="cdb"]').value;
+}
+
+function aliquot() {
+  return document.querySelector('input[name="aliquot"]:checked').value;
+}
+
+function calculateLCI() {
+  return cdbValue() / (1.0 - aliquot());
+}
+
+function calculateCDB() {
+  return lciValue() * (1.0 - aliquot());
+}
+
+function updateAll() {
+  cdbResult.value = `${calculateLCI().toFixed(2)}%`;
+  lciResult.value = `${calculateCDB().toFixed(2)}%`;
+  lci.value = calculateCDB();
+  cdb.value = calculateLCI();
+}
+
+function updateCDB(element) {
   lciResult.value = `${element.value}%`;
-  const result = element.value / (1.0-0.225);
+  const result = element.value / (1.0 - aliquot());
   cdb.value = result;
   cdbResult.value = `${result.toFixed(2)}%`;
 }
 
-function calculateCDB(element) {
+function updateLCI(element) {
   cdbResult.value = `${element.value}%`;
-  const result = element.value * (1.0-0.225);
+  const result = element.value * (1.0 - aliquot());
   lci.value = result;
   lciResult.value = `${result.toFixed(2)}%`;
 }
