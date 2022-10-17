ff1f92c52246352a0ad6dddbbcf10a97a9e5b2b3
Élisson Michael <elissonmichael@yahoo.com.br>
2022-10-17 10:21:15 -0300

    Limit to Two Decimal Places

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function calculateLCI(element) {
   lciResult.value = `${element.value}%`;
   const result = element.value / (1.0-0.225);
   cdb.value = result;
-  cdbResult.value = `${result}%`;
+  cdbResult.value = `${result.toFixed(2)}%`;
 }
 
 function calculateCDB(element) {
   cdbResult.value = `${element.value}%`;
   const result = element.value * (1.0-0.225);
   lci.value = result;
-  lciResult.value = `${result}%`;
+  lciResult.value = `${result.toFixed(2)}%`;
 }
