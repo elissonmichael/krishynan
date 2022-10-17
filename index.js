76b9b8c2ba744548fec53b788f959bf437ff2499
Élisson Michael <elissonmichael@yahoo.com.br>
2022-10-17 15:59:05 -0300

    Refactor to Remove Complexity

--- index.js
+++ index.js
@@ -2,25 +2,30 @@ function aliquot() {
   return document.querySelector('input[name="aliquot"]:checked').value;
 }
 
+function lciCalculation(cdbValue) {
+  return cdbValue / (1.0 - aliquot());
+}
+
+function cdbCalculation(lciValue) {
+  return lciValue * (1.0 - aliquot());
+}
+
 function resetAll() {
   let default_cdb = 80.0;
   cdbResult.value = `${default_cdb}%`;
   cdb.value = default_cdb;
-  let default_lci = default_cdb * (1.0 - aliquot());
-  lciResult.value = `${default_lci.toFixed(2)}%`;
-  lci.value = default_lci;
+  lci.value = cdbCalculation(default_cdb);
+  lciResult.value = `${lci.value}%`;
 }
 
 function updateCDB(element) {
   lciResult.value = `${element.value}%`;
-  const result = element.value / (1.0 - aliquot());
-  cdb.value = result;
-  cdbResult.value = `${result.toFixed(2)}%`;
+  cdb.value = lciCalculation(element.value);
+  cdbResult.value = `${cdb.value}%`;
 }
 
 function updateLCI(element) {
   cdbResult.value = `${element.value}%`;
-  const result = element.value * (1.0 - aliquot());
-  lci.value = result;
-  lciResult.value = `${result.toFixed(2)}%`;
+  lci.value = cdbCalculation(element.value);
+  lciResult.value = `${lci.value}%`;
 }
